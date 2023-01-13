aae6cbd414f0b758d0f647c8e68401263fc72a52
TanishqSingla <tanishqsingla00@gmail.com>
2023-01-13 06:01:29 +0530

    feat: cors enabled

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const chalk = require('chalk');
+const cors = require('cors');
 
 const userRouter = require('./routes/userRoutes.js');
 
 mongoose.connect("mongodb://127.0.0.1:27017/users").then(() => console.log(chalk.blue('db connected')));
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/user', userRouter);
